567b802cf7a8cf6d6c5b5cc0be0884cc0cfde247
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2021-08-08 21:34:40 +0200

    Spring main

---
+++ src/main/java/dnd/character/creator/CharacterCreatorApplication.java
@@ -0,0 +1,23 @@
+package dnd.character.creator;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CharacterCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterCreatorApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+	@Bean
+	public ObjectMapper objectMapper(){
+		return new ObjectMapper().findAndRegisterModules();
+	}
+}
