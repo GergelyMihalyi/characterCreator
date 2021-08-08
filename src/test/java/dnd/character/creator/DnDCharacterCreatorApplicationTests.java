3941679fb614e5b45a52d4cbfc8f93bc7d16e8eb
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2021-08-08 21:24:02 +0200

    initial config

---
+++ src/test/java/dnd/character/creator/DnDCharacterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dnd.character.creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDCharacterCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
