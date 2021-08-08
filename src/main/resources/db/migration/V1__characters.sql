afadb70931c6c55c244220c97a0eed4f4ba80ff8
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2021-08-08 21:32:04 +0200

    character database migration and test characters

---
+++ src/main/resources/db/migration/V1__characters.sql
@@ -0,0 +1,4 @@
+create table characters(id bigint auto_increment, name varchar(255), age int, armor_class int, base_attack int,health_point int, primary key(id));
+
+insert into characters(name,age,armor_class,base_attack,health_point) values ('John Doe',31,13,15,110);
+insert into characters(name,age,armor_class,base_attack,health_point) values ('Jack Doe',31,13,15,100);
