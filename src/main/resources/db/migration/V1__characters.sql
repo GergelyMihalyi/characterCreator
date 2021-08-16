b60894a3ec65654bc8a867697de83a3a6f8f07ca
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2021-08-16 17:55:43 +0200

    new tables sql

--- src/main/resources/db/migration/V1__characters.sql
+++ src/main/resources/db/migration/V1__characters.sql
@@ -3,14 +3,14 @@ create table weapons(id bigint auto_increment, name varchar(30) not null, damage
 insert into weapons(name,damage,weight,weapon_type) values ('Axe',10,10,'CUTTING');
 insert into weapons(name,damage,weight,weapon_type) values ('Mace',6,20,'CRUSHING');
 
-create table characters(id bigint auto_increment, name varchar(60) not null, age int, base_attack_damage int, base_health_point int, actual_health_point int not null, weapon_id bigint, primary key(id));
+create table characters(id bigint auto_increment, name varchar(60) not null, age int, base_attack_damage int, base_health_point int, actual_health_point int not null, weapon_id bigint,experience int not null,level int not null, primary key(id));
 
 
 ALTER TABLE characters
     ADD CONSTRAINT CHARACTER_WEAPON_ID_FK
         FOREIGN KEY (weapon_id) REFERENCES weapons(id);
-insert into characters(name,age,armor_class,base_attack,health_point) values ('John Doe',31,13,15,110);
-insert into characters(name,age,armor_class,base_attack,health_point) values ('Jack Doe',31,13,15,100);
+insert into characters(name,age,base_attack_damage,base_health_point,actual_health_point,experience,level) values ('John Doe',31,13,110,110,0,1);
+insert into characters(name,age,base_attack_damage,base_health_point,actual_health_point,experience,level) values ('Jack Doe',31,13,100,100,0,1);
 
 create table items(id bigint auto_increment, name varchar(30) not null, description text(500),  primary key(id));
 
@@ -45,7 +45,7 @@ ALTER TABLE character_race
         FOREIGN KEY (character_id) REFERENCES characters(id);
 
 ALTER TABLE character_race
-    ADD CONSTRAINT CHARACTER_RACE_CHARACTER
+    ADD CONSTRAINT CHARACTER_RACE_RACE
         FOREIGN KEY (race_id) REFERENCES items(id);
 
 
